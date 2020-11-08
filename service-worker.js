f0dcdf34bcc4a143f1e4ff9e4ba8329a9280a002
divyabhutani1 <73690711+divyabhutani1@users.noreply.github.com>
2020-11-08 05:53:07 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-covid-tracker-1/precache-manifest.a2038650d1e1aba7fc841f7bce7d0cac.js"
+  "/react-covid-tracker1/precache-manifest.76363079a1015fe749c0c92e4d37b0c1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-covid-tracker-1/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-covid-tracker1/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
